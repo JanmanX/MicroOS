c028a2f4c3a295b3a65f2794546e09607f46bf4d
Jan Meznik <jan@meznik.dk>
2017-02-28 08:55:01 +0100

    A lot of work has been done on xsdt, NOT FINISHED!
    Although this change is not working, it is committed due to the
    work put it.

--- os/init/xsdt.c
+++ os/init/xsdt.c
@@ -3,25 +3,122 @@
 #include "lib/klib.h"
 
 static rsdp_desc_t *rsdp;
+static xsdt_hdr_t *xsdt;
 
 
-long int get_rsdp();
-void xsdt_init()
+void dump_rsdp(rsdp_desc_t *r)
 {
-	rsdp = (rsdp_desc_t *)get_rsdp();
-	kprintf("RSDP at: 0x%x", (uint64_t)rsdp);
+	char buf[9] = {0};
+	memcpy(buf, &(r->signature), 8);
+	kprintf("RSDP--SIG: %s ", buf);
+	kprintf("CHKSM: 0x%x ", r->oemid);
+	memcpy(buf, &(r->oemid), 6);
+	kprintf("OEMID: %s ", buf);
+	kprintf("REV: 0x%x RSDT_ADDR: 0x%x ", r->revision, r->rsdt_address);
+}
 
+rsdp_desc_t *get_rsdp()
+{
+	uint32_t off;
 
+	uint64_t* search;
 
-	return 0;
+	/* Search in first KiB of EDBA
+	 * then  0x000E0000 to 0x000FFFFF
+	 */
+
+	/* Search a lot of memory */
+	for (search = (uint64_t*)0xA0000;
+	     (uint64_t)search < 0x100000;
+	     search += 0x10) {
+
+		/* Check if signature matches */
+		if (memcmp(search, "RSD PTR ", sizeof(uint64_t)) == 0) {
+			kprintf("Signature found at: 0x%x, rev: %d\n",
+				(uint64_t)search,
+				((rsdp_desc_t*)search)->revision);
+
+			rsdp_desc_t *r = (rsdp_desc_t*)search;
+			dump_rsdp(r);
+
+			if(r->revision != RSDP_REV_10 || r->revision !=
+			   RSDP_REV_20) {
+				continue;
+			}
+
+			DEBUG("RIGHT SIGNATURE");
+			HALT;
+
+			uint64_t len = r->revision == RSDP_REV_10 ?
+				RSDP_DESC_10_LEN : RSDP_DESC_20_LEN;
+
+			if(acpi_checksum((uint8_t*)r, len) == 0) {
+				return r;
+			}
+		}
+	}
+
+	return NULL;
 }
 
 
 
-uint8_t xsdt_checksum(acpi_xsdt_hdr_t *hdr)
+void xsdt_init()
 {
+	rsdp_desc_t *r2 = get_rsdp();
+	HALT;
+
+	rsdp = (rsdp_desc_t *)get_rsdp();
+	kprintf("RSDP at: 0x%x\n", (uint64_t)rsdp);
+
+	HALT;
+	kprintf("XSDT at: 0x%x\n", (uint64_t)xsdt);
+
+
+	kprintf("XSDT:\n");
+	clear_screen();
+	char str[9] = {0};
+
+	memcpy(str, &xsdt->oemid, 6);
+	kprintf("XSDT->OEMID: %s\n", str);
+	memcpy(str, &xsdt->oemtableid, 8);
+	kprintf("XSDT->OEMTABLEID: %s\n", str);
+
+
+
+
+	kprintf("First 4 bytes of XSDT: 0x%x\n", ((uint32_t*)xsdt)[0]);
+
+
+	kprintf("XSDT_CHECKSUM: 0x%x\n", xsdt_checksum(xsdt));
+
+
+	if(xsdt_checksum(xsdt) != 0) {
+		ERROR("XSDT checksum failed!\n");
+	}
+
+	HALT;
+	kprintf("XSDP at: 0x%x", (uint64_t)xsdt);
+	return 0;
+}
+
+
+uint8_t acpi_checksum(const uint8_t *data, uint64_t len)
+{
+	int i;
 	uint8_t sum = 0;
-	uint32_t i;
+
+	for (i = 0; i < len; i++) {
+		sum += data[i];
+	}
+
+	return sum;
+}
+
+uint8_t xsdt_checksum(xsdt_hdr_t *hdr)
+{
+	uint64_t sum = 0;
+	uint64_t i;
 	for(i = 0; i < hdr->length; i++) {
 		sum += ((uint8_t*)hdr)[i];
 	}
@@ -31,7 +128,7 @@ uint8_t xsdt_checksum(acpi_xsdt_hdr_t *hdr)
 
 void *xsdt_find_sdt(char* signature)
 {
-   return NULL;
+	return NULL;
 }
 
 
--- os/init/xsdt.h
+++ os/init/xsdt.h
@@ -3,26 +3,37 @@
 
 #include <stdint.h>
 
+#define RSDP_SIGNATURE "RSD PTR "
 
 /* Pointer to the XSDT */
+#define RSDP_REV_10 0
+#define RSDP_REV_20 2
+#define RSDP_DESC_10_LEN (20)
+#define RSDP_DESC_20_LEN (36)
+
+/* RSDP Version 1.0 */
 typedef struct rsdp_desc {
-	char signature[8];
+	uint8_t signature[8];
 	uint8_t checksum;
-	char oemid[6];
+	uint8_t oemid[6];
 	uint8_t revision;
 	uint32_t rsdt_address;
+}__attribute__((packed)) rsdp_desc_10_t;
+
+typedef struct rsdp_desc_20 {
+	/* First part */
+	rsdp_desc_10_t rsdp_10;
 
 	/* 2.0, extension */
 	uint32_t length;
 	uint64_t xsdt_address;
 	uint8_t ext_checksum;
 	uint8_t reserved[3];
-} __attribute__ ((packed)) rsdp_desc_t;
-
+} __attribute__ ((packed)) rsdp_desc_20_t;
 
 
 /* The RSDT is the main System Description Table */
-typedef struct acpi_xsdt_hdr {
+typedef struct xsdt_hdr {
 	char signature[4];
 	uint32_t length;
 	uint8_t revision;
@@ -32,11 +43,15 @@ typedef struct acpi_xsdt_hdr {
 	uint32_t oemrevision;
 	uint32_t creatorid;
 	uint32_t creatorrevision;
-} __attribute__((packed)) acpi_xsdt_hdr_t;
+} __attribute__((packed)) xsdt_hdr_t;
+
 
 
 void xsdt_init();
 
-//uint8_t xsdt_checksum(acpi_xsdt_hdr_t*);
+rsdp_desc_t *get_rsdp();
+uint8_t acpi_checksum(const uint8_t *data, uint64_t len);
+uint8_t rsdp_checksum(rsdp_desc_t *rsdp);
+uint8_t xsdt_checksum(xsdt_hdr_t*);
 //void *xsdt_find_sdt(char *signature);
 #endif /* _XSDT_H */
