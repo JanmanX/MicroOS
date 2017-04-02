27573993da59a808fb903bc97cfb63c7d65df2b7
Jan Meznik <jan@meznik.dk>
2017-04-02 18:24:29 +0200

    Changes in toolchains and configs.

--- .gdbinit
+++ .gdbinit
@@ -1,5 +1,12 @@
 set arch i386:x86-64:intel
 file os/kernel.bin
 target remote localhost:1234
-
 layout src
+
+
+
+# Temporary
+#watch *3221100764
+#break *xsdt_init
+#continue
+#find 0xE0000, 0x1000000, 0x2052545020445352
--- tools/download_tools.sh
+++ tools/download_tools.sh
@@ -1,3 +1,5 @@
+NJOBS=8
+
 mkdir ./src
 
 
@@ -6,14 +8,23 @@ wget http://download.qemu-project.org/qemu-2.8.0.tar.xz
 tar xvJf qemu-2.8.0.tar.xz
 cd qemu-2.8.0
 ./configure
-make
+make -j${NJOBS}
+cd ..
 
-
+# BOCHS
+wget https://downloads.sourceforge.net/project/bochs/bochs/2.6.8/bochs-2.6.8.tar.gz?r=&ts=1491134677&use_mirror=netcologne
+tar xf bochs-2.6.8.tar.gz
+cd bochs-2.6.8
+./configure --enable-debugger --enable-debugger-gui --enable-x86-debugger --with-sdl --enable-x86-64
+make -j${NJOBS}
+#sudo make install
+cd ..
 
 
 ############################### OPTIONAL #######################################
 # QBOOT
 # Better BIOS for QEMU
-wget https://github.com/bonzini/qboot.git
+git clone https://github.com/bonzini/qboot.git
+
 
 
